d6e50b279dfa27df580b44b7756c1b637f057979
ragu <rrrtechie@gmail.com>
2023-02-23 21:43:40 +0530

    file to bytes

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import matplotlib.patches as patches
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
 import numpy as np
+import json
+import base64
 from load import *
 
 # Initilaising app and wrapping it in CORS to allow request from different services
@@ -17,6 +19,8 @@ app = Flask(__name__)
 CORS(app)
 # Telling matplotlib to not create GUI Windows as our application is backend and doesn't require direct visulaization
 matplotlib.use('agg')
+
+
 # Loading our custom model
 # model = core.Model.load('model_weights.pth', ['rbc'])
 
@@ -28,7 +32,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def hello_world():
-    return 'Hello Megha!'
+    return 'Hello!'
 
 
 # Adding new POST endpoint that will accept image and output image with bounding boxes of detected objects
@@ -52,18 +56,22 @@ def test():
 def detect():
     model = init()
     # Accessing file from request
-    file = request.files['image']
+    # file = request.files['image']
+    data = request.json
+    image_rec = data['image']
 
-    # Check if the file extension is allowed
-    if not allowed_file(file.filename):
-        return "Only PNG and JPG image formats are allowed", 400
+    # Decode the Base64-encoded image data to bytes
+    image_bytes = base64.b64decode(image_rec)
+    image_byte = io.BytesIO(image_bytes)
 
-    image = Image.open(file).convert('RGB')
+    # Open the image data as an Image object
+    image1 = Image.open(image_byte).convert('RGB')
+
     torch_model = model.get_internal_model()
     if torch_model.roi_heads.detections_per_img == 100:
         torch_model.roi_heads.detections_per_img = 1000
     # Using model to detect objects
-    predictions = model.predict(image)
+    predictions = model.predict(image1)
     labels, boxes, scores = predictions
     # Applying threshold
     lab = []
@@ -75,20 +83,43 @@ def detect():
     box = torch.stack(box)
     # Creating figure and displaying original image
     fig, ax = plt.subplots(1)
+
+    # Set aspect ratio to equal the ratio of the image dimensions
+    # aspect_ratio = image1.width / image1.height
+    # ax.set_aspect(aspect_ratio)
+
     plt.axis('off')
-    ax.imshow(image)
+    ax.imshow(image1)
     # Adding bounding boxes
     for i in range(len(box)):
         ax.add_patch(
             patches.Rectangle((box[i][0], box[i][1]), box[i][2] - box[i][0], box[i][3] - box[i][1], linewidth=1,
                               edgecolor='r', facecolor='none'))
 
+    # fig.patch.set_alpha(0)
+    #
+    # # Set the DPI to the default value of 100
+    # fig.set_dpi(100)
+
     # Preparing output
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
 
-    # Sending response as png image
-    return Response(output.getvalue(), mimetype='image/png')
+    number = len(labels)
+
+    data = {
+        'image': base64.b64encode(output.getvalue()).decode('utf-8'),
+        'number': number
+    }
+
+    # Convert the dictionary to JSON
+    json_data = json.dumps(data)
+
+    # Return the JSON response
+    return Response(json_data, mimetype='application/json')
+
+    # # Sending response as png image
+    # return Response(output.getvalue(), mimetype='image/png')
 
 
 if __name__ == '__main__':
